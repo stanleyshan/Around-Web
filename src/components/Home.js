f70123bf220402b8d78b23fcec20ce83805d2bbb
ning <egg11272001@gmail.com>
2018-01-17 20:33:31 -0800

    loading geo location state finished

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,58 @@
 import React from 'react';
-import { Tabs, Button } from 'antd';
-
+import { Tabs, Button, Spin } from 'antd';
+import { GEO_OPTIONS } from '../constants';
 
 const TabPane = Tabs.TabPane;
 const operations = <Button>Extra Action</Button>;
 
 export class Home extends React.Component {
+    state = {
+        loadingGeoLocation: false,
+        error: ' ',
+    }
+
+    componentDidMount() {
+        this.setState({loadingGeoLocation: true, error: '' });
+        this.getGeoLocation();
+    }
+
+    getGeoLocation = () => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                this.onSuccessLoadGeoLocation,
+                this.onFailedLoadGeoLocation,
+                GEO_OPTIONS,
+            );
+        } else {
+            this.setState({ error: 'Your brower does not support geolocation!' });
+        }
+    }
+
+    onSuccessLoadGeoLocation = (position) => {
+        console.log(position);
+        this.setState({ loadingGeoLocation: false, error: '' });
+    }
+
+    onFailedLoadGeoLocation = () => {
+        this.setState({ loadingGeoLocation: false, error: 'Failed to load geo location!' });
+    }
+
+    getGalleryPanelContent = () => {
+        if (this.state.error) {
+            return <div>{this.state.error}</div>
+        } else if (this.state.loadingGeoLocation) {
+            return <Spin tip="Loading geo location..."/>
+        } else {
+            return null;
+        }
+    }
+
     render() {
         return (
             <Tabs tabBarExtraContent={operations} className="main-tabs">
-                <TabPane tab="Posts" key="1">Content of tab 1</TabPane>
+                <TabPane tab="Posts" key="1">
+                    {this.getGalleryPanelContent()}
+                </TabPane>
                 <TabPane tab="Map" key="2">Content of tab 2</TabPane>
             </Tabs>
         )
